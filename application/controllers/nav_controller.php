ad11ea94612615787fab5ee9aba667627a6f836e
athenluffy <31587653+athenluffy@users.noreply.github.com>
2017-09-03 17:24:44 +0530

    fv

--- application/controllers/nav_controller.php
+++ application/controllers/nav_controller.php
@@ -30,10 +30,6 @@ class Nav_controller extends CI_Controller {
 	    $this->load->view('utility/session_master');
 	}
 	
-	public function master_examtype(){
-		$this->load->view('utility/exam_type_master');
-	}
-	
 	public function master_role(){
 		$this->load->view('utility/role_master');
 	}
