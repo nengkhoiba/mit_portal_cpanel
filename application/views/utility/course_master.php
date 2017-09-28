69278ab16621c386efa21eaa1a659032bede5404
athenluffy <31587653+athenluffy@users.noreply.github.com>
2017-09-28 19:53:12 +0530

    master update

--- application/views/utility/course_master.php
+++ application/views/utility/course_master.php
@@ -21,7 +21,7 @@
      		
      			$msg=$this->session->userdata('status');
      			?>
-     			<div id="success-alert" class="alert alert-warning alert-dismissible" role="alert">
+     			<div id="success-alert" class="alert alert-success alert-dismissible" role="alert">
 				  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 				  <strong>Message: </strong> <?php echo $msg;?>
 				</div>
