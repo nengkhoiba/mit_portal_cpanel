09c9c0aec8e1bb20dd78e2b4051298ff089b52df
athenluffy <31587653+athenluffy@users.noreply.github.com>
2017-10-02 20:24:39 +0530

    landing

--- application/views/employee/landing.php
+++ application/views/employee/landing.php
@@ -1,15 +1,24 @@
  <?php $this->load->view('global/header.php');?>
-      <?php $this->load->view('global/side_menu.php');?>
+      <?php $this->load->view('global/side_menu.php');
+      $sid=$this->session->userdata('session');
+      ?>
        <div class="content-wrapper">
       <section class="content">
        <div class="row">
        <div class="col-md-3 col-sm-6 col-xs-12">
         <div class="info-box bg-yellow">
             <span class="info-box-icon"><i class="ion ion-ios-pricetag-outline"></i></span>
-
+              <?php 
+              $sql2="SELECT  `name` FROM `session` WHERE `id`=$sid";
+              $query2=$this->db->query($sql2);
+              while($result=mysql_fetch_array($query2->result_id))
+              {
+                  $sn=$result['name'];
+              }
+              ?>
             <div class="info-box-content">
-              <span class="info-box-text">Inventory</span>
-              <span class="info-box-number">5,200</span>
+              <span class="info-box-text">Current session</span>
+              <span class="info-box-number"><?php echo $sn;?></span>
 
               <div class="progress">
                 <div class="progress-bar" style="width: 50%"></div>
@@ -27,8 +36,16 @@
             <span class="info-box-icon"><i class="ion ion-ios-pricetag-outline"></i></span>
 
             <div class="info-box-content">
-              <span class="info-box-text">Inventory</span>
-              <span class="info-box-number">5,200</span>
+              <span class="info-box-text">Total No. of Students</span>
+              <?php 
+              $sql="SELECT * FROM `student_details`";
+              $query=$this->db->query($sql);
+              if($query)
+              {
+                  $n=$query->num_rows();
+              }
+              ?>
+              <span class="info-box-number"><?php echo $n;?></span>
 
               <div class="progress">
                 <div class="progress-bar" style="width: 50%"></div>
@@ -45,8 +62,16 @@
             <span class="info-box-icon"><i class="ion ion-ios-pricetag-outline"></i></span>
 
             <div class="info-box-content">
-              <span class="info-box-text">Inventory</span>
-              <span class="info-box-number">5,200</span>
+            <?php 
+              $sql1="SELECT * FROM `admission_std_relation` WHERE `session_id`=$sid";
+              $query1=$this->db->query($sql1);
+              if($query1)
+              {
+                  $a=$query1->num_rows();
+              }
+              ?>
+              <span class="info-box-text">Admitted Students</span>
+              <span class="info-box-number"><?php echo $a;?></span>
 
               <div class="progress">
                 <div class="progress-bar" style="width: 50%"></div>
@@ -63,8 +88,33 @@
             <span class="info-box-icon"><i class="ion ion-ios-pricetag-outline"></i></span>
 
             <div class="info-box-content">
-              <span class="info-box-text">Inventory</span>
-              <span class="info-box-number">5,200</span>
+            <?php
+        $sql4="SELECT `id`, `abv`, `isActive` FROM `course` WHERE `isActive`=1";
+        $query4=$this->db->query($sql4);
+        if($query4)
+        {
+            while($result1=mysql_fetch_array($query4->result_id))
+            {
+                $cid=$result1['id'];
+                $name=$result1['abv'];
+                $sql5="SELECT * FROM `std_col_relation` WHERE `course_id`=$cid AND `isActive`=1";
+                $query5=$this->db->query($sql5);
+                if($query5)
+                {
+                    
+                        $ns=$query5->num_rows();
+                    
+                
+                ?>
+              <span class="info-box-text"><?php echo $name;?></span>
+              <span class="info-box-number"><?php echo $ns;?></span>
+              <?php 
+                    
+                }
+            }
+        }
+        ?>
+            
 
               <div class="progress">
                 <div class="progress-bar" style="width: 50%"></div>
@@ -117,48 +167,53 @@
                   <!-- /.chart-responsive -->
                 </div>
                 <!-- /.col -->
-                <div class="col-md-4">
-                  <p class="text-center">
-                    <strong>Goal Completion</strong>
-                  </p>
-
-                  <div class="progress-group">
-                    <span class="progress-text">Add Products to Cart</span>
-                    <span class="progress-number"><b>160</b>/200</span>
+                <?php 
+					$sql6="SELECT `id`, `abv`, `isActive` FROM `course` WHERE `isActive`=1";
+					$query6=$this->db->query($sql6);
+					if($query6)
+					{
+					    while($result2=mysql_fetch_array($query6->result_id))
+					    {
+					        $coid=$result2['id'];
+					    ?>
+					    <div class="col-md-4">
+					    <p class="text-center">
+					    <strong><?php echo $result2['abv'];?></strong>
+					    </p>
+					<?php
+                            $sql7="SELECT `id`, `abv` FROM `trade` WHERE `isActive`=1";
+                            $query7=$this->db->query($sql7);
+                            if($query7)
+                            {
+                                
+                                while($result3=mysql_fetch_array($query7->result_id))
+                                {
+                                    $tid=$result3['id'];
+                                    $sql9="SELECT * FROM `std_col_relation` WHERE `course_id`=$coid AND `isActive`=1";
+                                    $query9=$this->db->query($sql9);
+                                    $totalcount=$query9->num_rows();
+					?>
+					<div class="progress-group">
+                    <span class="progress-text"><?php echo $result3['abv'];?></span>
+                    <?php
+                    $sql8="SELECT * FROM `std_col_relation` WHERE `course_id`=$coid AND `trade_id`=$tid AND `isActive`=1";
+                    $query8=$this->db->query($sql8);
+                    $count=$query8->num_rows();
+                   
+                    ?>
+                    <span class="progress-number"><b><?php echo $count;?></b></span>
 
                     <div class="progress sm">
-                      <div class="progress-bar progress-bar-aqua" style="width: 80%"></div>
+                      <div class="progress-bar progress-bar-aqua" style="width:<?php echo ($count/$totalcount)*100;?>%"></div>
                     </div>
                   </div>
-                  <!-- /.progress-group -->
-                  <div class="progress-group">
-                    <span class="progress-text">Complete Purchase</span>
-                    <span class="progress-number"><b>310</b>/400</span>
-
-                    <div class="progress sm">
-                      <div class="progress-bar progress-bar-red" style="width: 80%"></div>
-                    </div>
-                  </div>
-                  <!-- /.progress-group -->
-                  <div class="progress-group">
-                    <span class="progress-text">Visit Premium Page</span>
-                    <span class="progress-number"><b>480</b>/800</span>
-
-                    <div class="progress sm">
-                      <div class="progress-bar progress-bar-green" style="width: 80%"></div>
-                    </div>
-                  </div>
-                  <!-- /.progress-group -->
-                  <div class="progress-group">
-                    <span class="progress-text">Send Inquiries</span>
-                    <span class="progress-number"><b>250</b>/500</span>
-
-                    <div class="progress sm">
-                      <div class="progress-bar progress-bar-yellow" style="width: 80%"></div>
-                    </div>
-                  </div>
-                  <!-- /.progress-group -->
-                </div>
+					<?php 
+					}
+                            }
+					    }
+					}
+					?>
+               
                 <!-- /.col -->
               </div>
               <!-- /.row -->
@@ -182,7 +237,7 @@
                     <span class="description-text">TOTAL COST</span>
                   </div>
                   <!-- /.description-block -->
-                </div> 
+                </div>
                 <!-- /.col -->
                 <div class="col-sm-3 col-xs-6">
                   <div class="description-block border-right">
